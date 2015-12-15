63a1efc9bd5c4e37bb8efccb43805dd0061e005e
simplon-gwenG <lambertleila@aol.com>
2015-12-15 10:24:03 +0100

    3 eme envoi

--- jq.js
+++ jq.js
@@ -1,9 +1,21 @@
 $(document).ready (function(){
-
+	$("#recup").hide();
+	$("#pswd_info").hide();
 	   $("#recupMdp").click(function(){
 		   $("#login").hide();
 		   $("#MDP").hide();
+		   $("#recup").show();
 		   return false;
 	   })
+	  
 });
+ function recupValeur(valeurChamp) {
+
+if	(valeurChamp.length<6){
+	$("#pswd_info").show();
+	return false;
+}
+
+};
+
 	
